be9dfb61da8d2788c8c6c4cb754c253ddaf8fafd
Allen Román <2909080001301@ingenieria.usac.edu.gt>
2023-12-19 03:10:21 -0600

    Consultas

--- Practica2/cli_hospital/src/views/consultas.js
+++ Practica2/cli_hospital/src/views/consultas.js
@@ -26,8 +26,49 @@ const consulta = async (connection, tabla) => {
                 message: "Presione enter para continuar...",
             },
         ]);
-        await menuCrud(connection);
+        await menuCrud(1,connection);
     }
+
+    //Realizar consulta de los ultimos 25 registros
+    console.log(chalk.bgBlue.bold("\n---------------- CONSULTA ----------------"));
+    
+    if (tabla == "paciente") {
+        var consulta = await connection.query(`SELECT * FROM bd2_practica2.paciente ORDER BY idPaciente DESC LIMIT 25`);
+    } else if (tabla == "habitacion") {
+        var consulta = await connection.query(`SELECT * FROM bd2_practica2.habitacion ORDER BY idHabitacion DESC LIMIT 25`);
+    } else if (tabla == "log_actividad") {
+        var consulta = await connection.query(`SELECT * FROM bd2_practica2.log_actividad ORDER BY id_log_actividad DESC LIMIT 25`);
+    } else if (tabla == "log_habitacion") {
+        var consulta = await connection.query(`SELECT * FROM bd2_practica2.log_habitacion ORDER BY idHabitacion DESC LIMIT 25`);
+    }
+
+    
+    const resultadosFormateados = consulta[0].map(fila => {
+        const filaFormateada = { ...fila };
+        Object.keys(filaFormateada).forEach(key => {
+          if (typeof filaFormateada[key] === 'string') {
+            filaFormateada[key] = filaFormateada[key].replace(/\r/g, ''); // Reemplazar \r con cadena vacía
+          }
+        });
+        return filaFormateada;
+      });
+      
+    console.table(resultadosFormateados);
+
+    await poolLog.query(`INSERT INTO bd2_practica2.log_operaciones_bd (accion, usuario) VALUES (?, ?)`, [`Realizó una consulta a la tabla ${tabla}`, session.user]);
+
+
+    //Regresar al crud
+    await inquirer.prompt([
+        {
+            type: "input",
+            name: "continue",
+            message: "Presione enter para continuar...",
+        },
+    ]);
+    //Limpiar la consola
+    clear();
+    await menuCrud(1,connection);
 
 
 };
