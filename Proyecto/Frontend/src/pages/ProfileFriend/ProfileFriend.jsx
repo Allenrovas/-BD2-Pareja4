c97b37add3b5f0265e8187bbf81ad59931b7619f
Allen Román <2909080001301@ingenieria.usac.edu.gt>
2023-12-28 00:17:49 -0600

    ProfileFriend

---
+++ Proyecto/Frontend/src/pages/ProfileFriend/ProfileFriend.jsx
@@ -0,0 +1,209 @@
+import React, { useEffect, useState } from "react";
+import { FaUserLarge } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
+import Service from "../../Service/Service";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import pdfSvg from "../../assets/pdfsvg.svg";
+const ProfileFriend = () => {
+  const navigate = useNavigate();
+
+  const usuario = JSON.parse(localStorage.getItem("data_user"));
+  const [listaCasos, setListaCasos] = useState([]);
+  const [nombreString, setNombreString] = useState("");
+  const [userDetails, setUserDetails] = useState({
+    name1: "",
+    name2: "",
+    lastname1: "",
+    lastname2: "",
+    username: "",
+    age: "",
+    email: "",
+    name: "",
+    specialty: "",
+    webSite: "",
+  });
+  useEffect(() => {
+    if (!usuario) {
+      navigate("/");
+    }
+
+    obtenerUsuario();
+    obtenerCasos();
+  }, []);
+  console.log(userDetails.password)
+  const obtenerUsuario = async () => {
+    try {
+      const data = JSON.parse(localStorage.getItem("data_user"));
+      const res = await Service.getUser(data.id);
+      if (res.status === 200) {
+        setUserDetails(res.data.data);
+        setNombreString(res.data.data.name1 + " " + res.data.data.name2+" "+res.data.data.lastname1+" "+res.data.data.lastname2);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+const obtenerCasos = async () => {
+  try {
+    //Obtener nombre de los pdf
+    const data = JSON.parse(localStorage.getItem("data_user"));
+    const res = await Service.getFiles(data.id);
+    console.log(res.data.data);
+    if (res.status === 200) {
+      setListaCasos(res.data.data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const obtenerPdf = (buffer) => {
+  try {
+    // Crear un Uint8Array desde el buffer
+    const uint8Array = new Uint8Array(buffer);
+
+    // Crear un Blob a partir del Uint8Array
+    const blob = new Blob([uint8Array], { type: "application/pdf" });
+
+    // Crear una URL para el Blob
+    const pdfUrl = URL.createObjectURL(blob);
+
+    return pdfUrl;
+  } catch (error) {
+    console.error("Error al crear el PDF:", error);
+    return null;
+  }
+};
+
+
+
+
+  return (
+    <div className="flex bg-zinc-900">
+      <Sidebar />
+      <div className="p-7 text-2xl font-semibold flex-1 h-screen overflow-y-scroll scrollbar-hide">
+        <div>
+          <h1 className="text-white text-3xl">
+            <FaUserLarge className="text-3xl inline-block mr-2" />
+            Mi Perfil
+          </h1>
+        </div>
+        <div class="flex items-center justify-center p-12">
+          <div class="mx-auto w-full max-w-[550px]">
+            <form>
+              <div class="mb-5">
+                <label class="mb-3 block text-base font-semibold text-white sm:text-xl">
+                  Nombre Completo
+                </label>
+                <div class="-mx-3 flex flex-wrap">
+                  <div class="w-full px-3 sm:w-1/2">
+                    <div class="mb-5">
+                      <h2 className="text-4xl text-white font-light">
+                        {nombreString}
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="mb-5">
+                <label
+                  for="phone"
+                  class="mb-3 block text-base font-medium text-white"
+                >
+                  Nombre de Usuario
+                </label>
+                <h2 className="text-4xl text-white font-light">
+                  {userDetails.username}
+                </h2>
+              </div>
+              <div class="mb-5">
+                <label
+                  for="email"
+                  class="mb-3 block text-base font-medium text-white"
+                >
+                  Dirección de correo electrónico
+                </label>
+                <h2 className="text-4xl text-white font-light">
+                    {userDetails.email}
+                </h2>
+              </div>
+
+              <div class="mb-5">
+                <label
+                  class="mb-3 block text-base font-medium text-white"
+                >
+                  Sitio Web
+                </label>
+                <h2 className="text-4xl text-white font-light">
+                  {userDetails.webSite}
+                </h2>
+              </div>
+
+              <div class="mb-5">
+                <label
+                  class="mb-3 block text-base font-medium text-white"
+                >
+                  Especialidad
+                </label>
+                <h2 className="text-4xl text-white font-light">
+                  {userDetails.specialty}
+                </h2>
+              </div>
+
+              <div class="mb-5">
+                <label
+                  class="mb-3 block text-base font-medium text-white"
+                >
+                  Edad
+                </label>
+                <h2 className="text-4xl text-white font-light">
+                  {userDetails.age}
+                </h2>
+              </div>
+
+              
+            </form>
+          </div>
+        </div>
+        
+        
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-white text-3xl mb-6">
+            Mis Casos que estoy trabajando en formato
+          </h1>
+
+          <ul className="w-full">
+            {listaCasos.map((item, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-between border-2 py-2 px-3 rounded-2xl mb-4"
+              >
+                <div>
+                  <img src={pdfSvg} className="w-10 mr-2" alt="" />
+                </div>
+                <div>
+                  <h1 className="text-white text-xl">{item.name}</h1>
+                </div>
+                <div>
+                  <a
+                    href={obtenerPdf(item.content.data)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white text-xl"
+                  >
+                    Ver
+                  </a>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default ProfileFriend;
